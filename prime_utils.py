e223812683d649dca4bf65af6b7119f71edf1431
Emhyr713 <kilmiskinn@gmail.com>
2024-11-10 15:03:42 +0300

    Update prime_utils.py

--- prime_utils.py
+++ prime_utils.py
@@ -6,7 +6,7 @@ def is_prime(n):
         return False
     if n <= 3:
         return True
-    if n % 2 == 0 or n % 3 == 0:
+    if n % 2 == 0 or n % 3 == 0: 
         return False
     # Проверяем делители от 5 до √n, пропуская чётные числа
     for i in range(5, int(n**0.5) + 1, 6):
